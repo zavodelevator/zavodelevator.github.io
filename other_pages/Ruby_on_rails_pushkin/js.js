55aaedaacfeb89254b258cc4dc76e8668ad9ba99
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2020-09-25 11:33:45 +0300

    цукенгш

--- other_pages/Ruby_on_rails_pushkin/js.js
+++ other_pages/Ruby_on_rails_pushkin/js.js
@@ -1,13 +1,30 @@
 
+ 
+//  var yy = $(window).scrollTop();  top position acros jquery
+// var posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop; //your current y position on the page
+
 
 
 let zakladkaButton = document.getElementById("zakladka")
 
 zakladkaButton.addEventListener('click', function (event) {
-    var posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    var posTop = $(window).scrollTop(); //your current y position on the page
+
 
     alert(posTop);
 
 });
 
 
+
+
+let zakladkaButton2 = document.getElementById("zakladka1")
+
+zakladkaButton2.addEventListener('click', function (event) {
+
+var posTop2 = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop; //your current y position on the page
+   
+
+$(window).scrollTop(177838);
+
+});
