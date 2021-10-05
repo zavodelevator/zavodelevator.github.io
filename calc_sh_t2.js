1c3810d718560ac11e91bc90d04f3ee3f74266f7
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2021-10-05 17:00:06 +0300

    DDD

--- calc_sh_t2.js
+++ calc_sh_t2.js
@@ -1,10 +1,4 @@
-// function calck_price() {
-//     // document.getElementById("demo").style.color = "red";
-//     console.log('Changed option value ' + $('#exampleFormControlSelect1 option:selected').val());
-//     console.log("fffffrr");
-    
-//     // $('#selectorId option:selected').val()
-// }
+
 
 $( "#calck_price" ).click(function() {
     let l_trans = $('#length_trans option:selected').val()
@@ -13,12 +7,8 @@ $( "#calck_price" ).click(function() {
     
     let price_sh_transporter = (params_trans[2]*1)+(params_trans[1]*1)+(params_trans[3]*l_trans*1.1)+(engine_params[2]*1);
 
-    // console.log(params_trans[2]*1)
-    // console.log(params_trans[1]*1)
-
-    $(".price").html( price_sh_transporter);
-    //    
+ 
+    $(".price").html( price_sh_transporter);  
   });
 
-  // 
-// console.log('Changed option value ' + $('#exampleFormControlSelect1 option:selected').val());
+  
