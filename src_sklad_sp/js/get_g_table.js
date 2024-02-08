ac52efe5295d82ecb0e61761cd2b2a91aa37a137
zavodelevator <68054345+zavodelevator@users.noreply.github.com>
2024-02-08 11:26:18 +0200

    uuu

--- src_sklad_sp/js/get_g_table.js
+++ src_sklad_sp/js/get_g_table.js
@@ -8,10 +8,10 @@ $(document).ready(function(){
     });
             
       $.ajax({
-        url: "https://script.google.com/macros/s/AKfycby1pO_94xtHo1m6LhtMEB8RlsBt6XkIq3FNSgTktGHn0xHmTrPjuJiNLy0CminF7t-YtQ/exec",
+        url: url,
         dataType: "json",
         success: function(data) {
-          localStorage.setItem('sclad_sp_json', JSON.stringify(data)); 
+          localStorage.setItem(table_name, JSON.stringify(data)); 
         },
         error: function(xhr, status, error) {            
           console.error("Error fetching data:", error);
