1cd3150a9712a0c84e2d45e864af26e4282bd6e1
zavodelevator <68054345+zavodelevator@users.noreply.github.com>
2026-01-15 10:41:58 +0200

    shkrebok

---
+++ src/calc_sh_t/src/vue.js
@@ -0,0 +1,48 @@
+new Vue({
+    el: '#distributorApp',
+    data: {
+      hasDistributor: false,
+      distributorLength: null,
+      data_shkr: [],
+      tables: [
+        {
+          name: "MR",
+          url: "https://script.google.com/macros/s/AKfycbzMg_jaGqQkMSqjXQAbhsvaCc16n-Cn2513om5-vNgU-uFJL4Z-gFpoXSkQgev5stsB/exec"
+        },
+        {
+          name: "tsh_parts_price",
+          url: "https://script.google.com/macros/s/AKfycbwgvuaBSBb-EEkWGwFCV0gYjOcJ7e94Lko8qmCh2qrniXMVFpAomypQJQvjSBCm6q4L/exec"
+        },
+        {
+          name: "tsh_leangth_kWt",
+          url: "https://script.google.com/macros/s/AKfycbxNZn_bJqPj7QuhZJC-gkzEWW3NauE5cAySfQ_nhEoX3kB0dRGatP3i_0m_qd7fWbQIhA/exec"
+        }
+      
+      ]
+    },
+    computed: {
+        currentImage() {
+            return this.hasDistributor ? 'src/calc_sh_t/src/img/Scraper_conveyor2.png' : 'src/calc_sh_t/src/img/banner22.jpg'
+        }
+    },
+    methods: {
+      processData() {
+        this.tables.forEach(item => {
+          $.ajax({
+            url: item.url,
+            dataType: "json",
+            success: (data) => {
+              this.data_shkr.push(item.name, data);
+              console.log('Отримані дані:', this.data_shkr);
+            },
+            error: (xhr, status, error) => {
+              console.error("Помилка при отриманні даних:", status, error);
+            }
+          });
+        });
+      }
+    },
+    mounted() {
+      this.processData();
+    }
+  })
