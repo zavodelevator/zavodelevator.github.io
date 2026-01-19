d7e56f2c738d8eb268144e1c2a43ed7689ae49bc
zavodelevator <68054345+zavodelevator@users.noreply.github.com>
2026-01-19 11:52:08 +0200

    Update calc_sh_t2.js

--- calc_sh_t2.js
+++ calc_sh_t2.js
@@ -289,9 +289,9 @@ function build_select_engin() {
 function prace_vue() {
   // перевірка та обрахунок на моторі та мотор-редукторі
   if(chek_chose_radio == false){
-      price_sh_transporter = ((params_trans[2]*1)+(params_trans[1]*1)+(params_trans[3]*l_trans*1.1)*1.2)+(engine_params[2]*1);
+      price_sh_transporter = ((params_trans[2]*1)+(params_trans[1]*1)+(params_trans[3]*l_trans*1.1));
     }else{
-      price_sh_transporter = (3000 + (params_trans[1]*1)+(params_trans[3]*l_trans*1.1)*1.2);
+      price_sh_transporter = (3000 + (params_trans[1]*1)+(params_trans[3]*l_trans*1.1));
     }
 
     
